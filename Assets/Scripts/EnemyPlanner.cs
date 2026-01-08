c76e4d3a4fb1fe1ff6e8990f7a910e0d39035a60
Vitaliis <Vitalik12-14@mail.ru>
2026-01-08 13:02:34 +0300

    Add infinitely possible Spawn Waves

--- Assets/Scripts/EnemyPlanner.cs
+++ Assets/Scripts/EnemyPlanner.cs
@@ -1,17 +1,59 @@
 using UnityEngine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyPlanner : MonoBehaviour
 {
-    [SerializeField] private WavePlanner _wave;
+    [SerializeField] private WavePlanner[] _waves;
+
+    private List<Enemy> _enemies;
+    private Coroutine _coroutine;
+
+    private bool _waveDestroyed;
 
     public event Func<Vector2, Enemy> Spawning;
 
+    private void OnDisable()
+        => StopCoroutine(_coroutine);
+
     private void Start()
     {
-        int enemyAmount = _wave.EnemyCount;
+        _enemies = new List<Enemy>();
+        _coroutine = StartCoroutine(SpawnAll());
 
-        for(int i = 0; i < enemyAmount; i++)
-            Spawning?.Invoke(_wave[i]);
+        _waveDestroyed = true;
+    }
+
+    private void FixedUpdate()
+    {
+        _enemies.RemoveAll(enemy => enemy.gameObject.activeSelf == false);
+        _waveDestroyed = _enemies.Count == 0;
+    }
+
+    private IEnumerator SpawnAll()
+    {
+        WaitUntil waiting = new(() => _waveDestroyed);
+
+        for(int i = 0; i < _waves.Length; i++)
+        {
+            Spawn(i);
+
+            yield return waiting;
+        }
+    }
+
+    private void Spawn(int waveIndex)
+    {
+        Enemy enemy;
+        int enemyAmount = _waves[waveIndex].EnemyCount;
+
+        for (int i = 0; i < enemyAmount; i++)
+        {
+            enemy = Spawning?.Invoke(_waves[waveIndex][i]);
+            _enemies.Add(enemy);
+        }
+
+        _waveDestroyed = false;
     }
 }
